f0974f6c031ff9e78933933e977dbe47e60111cf
itzjacki <31799931+itzjacki@users.noreply.github.com>
2018-12-17 01:35:51 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,26 @@
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function populateGallery(imageCount, albumName, albumElement){
+    for (var i = 0; i < imageCount; i++){
+        albumElement.innerHTML += `
+            <div class="singleImageParent"><div style="background-image: url(img/${albumName}/${i+1}.webp)" class="singleImage" onclick="viewImage(${i+1})"></div></div>
+        `;
+    } 
+}
+
+function viewImage(imageID){
+    modal.style.display = "flex";
+    imageShower.innerHTML = `
+        <span class="close" onclick="closeImage()">&times;</span>
+        <img class="popupImage" src="img/${albumName}/${imageID}.webp">
+    `
+}
+
+function closeImage(){
+    modal.style.display = "none";
+}
